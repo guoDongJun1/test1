31cb929e201c69a3b9b9d95951186bb75f76dca1
guoDongJun1 <hanling4015@163.com>
2020-11-14 16:51:19 +0800

    Tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-    dfsaf
+    <Tabbar></Tabbar>
   </div>
   <router-view/>
 </template>
+
+<script>
+  import Tabbar from './components/common/Tabbar/Tabbar'
+
+  export default {
+    name: "App",
+    components:{
+      Tabbar
+    }
+  }
+</script>
 
 <style>
 @import "assets/css/base.css";
---
+++ src/components/common/Tabbar/Tab-bar-item.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="tab-bar-item" @click="itemClick">
+<!--        <img src="../../assets/img/2.png" alt="">-->
+<!--        <div>首页</div>-->
+        <slot v-if="!isActived" name="slot-img"></slot>
+        <slot v-else name="slot-active-img"></slot>
+        <div :style="activeStyle">
+            <slot name="slot-text" ></slot>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Tab-bar-item",
+        data(){
+            return{
+                //isActived:true
+            }
+        },
+        computed:{
+          isActived(){
+              return this.$route.path.indexOf(this.path)!==-1
+          },
+            activeStyle(){
+              return this.isActived?{color:this.activeColor}:{}
+            }
+        },
+        props:{
+            path:String,
+            activeColor: {
+                type:String,
+                default:'deesSkyblue'
+            }
+        },
+        methods:{
+            itemClick(){
+                // console.log('klk')
+                this.$router.push(this.path)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    /*@import "../../assets/css/base.css";*/
+    .tab-bar-item{
+        text-align: center;
+        height: 49px;
+    }
+</style>
